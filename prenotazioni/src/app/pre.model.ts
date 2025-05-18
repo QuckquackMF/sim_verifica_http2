d0e3f0630399679c3a40c88cb36d47f92ee9d14e
QuckquackMF <toci.iljas@iisgalvanimi.edu.it>
2025-05-18 15:18:42 +0000

    punto 2

---
+++ prenotazioni/src/app/pre.model.ts
@@ -0,0 +1,19 @@
+export class Profili {
+    nome: string;
+    cognome: string;
+    indirizzo: string;
+    telefono: number;
+    email: string;
+    data: Date;
+    ora: number;
+
+    constructor(nome: string, cognome: string, indirizzo: string, telefono: number, email: string, data: Date, ora: number){
+        this.nome = nome
+        this.cognome = cognome
+        this.indirizzo = indirizzo
+        this.telefono = telefono
+        this.email = email
+        this.data = data
+        this.ora = ora
+    }
+}
