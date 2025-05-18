b20d5e8a193869ec06d62d98c9ea2d2a37fa6fc8
QuckquackMF <toci.iljas@iisgalvanimi.edu.it>
2025-05-18 15:33:17 +0000

    punto 3

--- prenotazioni/src/app/app.component.ts
+++ prenotazioni/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profili } from './pre.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Profili],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prenotazioni';
+  profile!: Profili[];
+  nuovidati!: Profili;
+
+  aggiungiDati(nome:HTMLInputElement, cognome: HTMLInputElement, indirizzo: HTMLInputElement, telefono: HTMLInputElement, email: HTMLInputElement, data: HTMLInputElement, ora: HTMLInputElement) {
+    const dataValue = new Date(data.value);
+    
+    this.nuovidati = new Profili(nome.value, cognome.value, indirizzo.value, parseInt(telefono.value), email.value, dataValue, parseFloat(ora.value))
+  }
 }
+
